a14ab7b3875ded3a5f36f4a24c8b04da086d65df
Abhishek Prajapati <bhishekprajapati@gmail.com>
2023-01-04 06:43:19 +0530

    initial

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    fontFamily: {
+      sans: ['League Spartan', 'sans-serif'],
+    },
+    colors: {
+      'gray-dark': 'hsl(0, 0%, 63%)',
+      'gray-darker': 'hsl(0, 0%, 27%)',
+      black: 'hsl(0, 0%, 0%)',
+      white: 'hsl(0, 0%, 100%)',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
